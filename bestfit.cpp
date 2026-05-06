e5369c6a38f115027cf0d298b92939923f849545
feb213 <dineshdhumal.fe@gmail.com>
2026-05-06 22:24:06 +0530

    Implement Best Fit Memory Allocation Algorithm

---
+++ bestfit.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+void bestFit(int blockSize[], int m, int processSize[], int n)
+{
+    int allocation[n];
+
+    // Initially no allocation
+    for(int i = 0; i < n; i++)
+        allocation[i] = -1;
+
+    // Allocate processes
+    for(int i = 0; i < n; i++)
+    {
+        int bestIndex = -1;
+
+        for(int j = 0; j < m; j++)
+        {
+            if(blockSize[j] >= processSize[i])
+            {
+                if(bestIndex == -1 || blockSize[j] < blockSize[bestIndex])
+                    bestIndex = j;
+            }
+        }
+
+        if(bestIndex != -1)
+        {
+            allocation[i] = bestIndex;
+            blockSize[bestIndex] -= processSize[i];
+        }
+    }
+
+    cout << "\nBest Fit Allocation:\n";
+
+    for(int i = 0; i < n; i++)
+    {
+        if(allocation[i] != -1)
+            cout << "Process " << i+1 << " -> Block " << allocation[i]+1 << endl;
+        else
+            cout << "Process " << i+1 << " -> Not Allocated" << endl;
+    }
+}
+
+int main()
+{
+    int blockSize[] = {100, 500, 200, 300, 600};
+    int processSize[] = {212, 417, 112, 426};
+
+    int m = 5;
+    int n = 4;
+
+    bestFit(blockSize, m, processSize, n);
+
+    return 0;
+}
